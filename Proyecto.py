c3d170b7a5d77c283fea7ee39aa123c9ce4181f6
noone <rafaelmostachoancjp@gmail.com>
2022-03-15 00:49:50 -0600

    Update README.md

--- Proyecto.py
+++ Proyecto.py
@@ -2,23 +2,50 @@
 from datetime import datetime
 
 # Opening a file
-file1 = open('data.txt', 'w')
+file1 = open('portfolio.txt', 'w')
 
-actionName = input("Cuál es el nombre de la acción: ")
-file1.writelines("Precio: "+actionName+"\n")
+actionType = input("Qué tipo de producto compraste\n 1* Forward\n 2* Call/Put\n 3* Nota Estructurada\n")
 
-price = input("Cuál fue el precio de la acción: ")
-file1.writelines("Precio: "+price+"\n")
+def forward():
+    file1.writelines("Forward \n")
+    actionName = input("Cuál es el nombre de la acción: ")
+    file1.writelines("Nombre: "+actionName+"\n")
 
-fee = input("Cuál fue la comisión: ")
-file1.writelines("Comisión: "+fee+"\n")
+    price = input("Cuál fue el precio de la acción: ")
+    file1.writelines("Precio: "+price+"\n")
 
-dateIf = input("Compraste la acción hoy? ")
-if dateIf == "y":
-    ndate = datetime.today().strftime('%Y-%m-%d')
-    file1.writelines("Fecha de Compra: "+ndate+"\n")
+    fee = input("Cuál fue la comisión: ")
+    file1.writelines("Comisión: "+fee+"\n")
+
+    dateIf = input("Compraste la acción hoy? ")
+    if dateIf == "y":
+        ndate = datetime.today().strftime('%Y-%m-%d')
+        file1.writelines("Fecha de Compra: "+ndate+"\n")
+    else:
+        date = input("Dime la fecha de cuando compraste la acción: ")
+        file1.writelines("Fecha de Compra: "+date+"\n")
+
+    file1.writelines("\n")
+    file1.writelines("\n")
+
+def call():
+    print("call")
+
+def put():
+    print("put")
+
+def notaEstructurada():
+    print("Nota Estructurada")
+
+
+if actionType == "1":
+    forward()
+elif actionType == "2":
+    call()
+elif actionType == "3":
+    notaEstructurada()
 else:
-    date = input("Dime la fecha de cuando compraste la acción: ")
-    file1.writelines("Fecha de Compra: "+date+"\n")
+    print("Ese producto no existe")
+
 
 file1.close()
--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Para este primer parcial lo que hice fue un programa simple que escriba en un do
 
 ## Analisis de primer parcial
 > Para este primer parcial solo hubo una acción que compre que fue el S&P 500 Index que es tiene un ES con el que puedes comprar derivatives como Futuros, entonces hice un Short sin ningún tipo de leverage. Estuve pensando en hacer esta operación desde hace ya un tiempo por que si analizamos la siguiente gráfica se puede ver claramente como hay un downtrend desde enero de 2022 pero aprete el botón en el momento en el que se desato el conflicto armado entre Rusia y Ukrania, no me preocupaba que hubiera volatilidad por que el conflicto no ha terminado y la inflación de este año de USA fue de poco más de 7% por lo que mi unica invalidación hubiera sido que el downtrend se rompiera y se mantuviera por más de un dia o sobrepasara alguno de los anteriores local top 
+<img width="1440" alt="Screen Shot 2022-03-15 at 0 46 51" src="https://user-images.githubusercontent.com/21222851/158322429-e6a27167-4c49-444b-9a3d-17cc81896093.png">
+
 ## Siguientes pasos
  * Agregar funcionalidad de strike price en mi Proyecto de Python conectandome al API de TradingView para así poder obtener más precisión en las fechas de Compra y Venta
  * No planeo cerrar mi posición hasta que el precio de S&P 500 rompa el down trend y se mantenga durante al menos un día, ese será mi SL en caso de que eso no suceda no tengo necesidad de abandonar mi posición hasta que el conflicto entre Ukrania y Rusia se detenga o alcance el precio de 3900-3800 dolares lo que me daría una ganancia de un 6 a 7% de mi capital.
+ <img width="1440" alt="Screen Shot 2022-03-15 at 0 47 22" src="https://user-images.githubusercontent.com/21222851/158322470-c48202db-b464-45b8-bd71-f5b7c9b53f0f.png">
