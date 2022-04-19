c6953e788d8b4752b1b0ad497c881b330ab3eb4f
Rafael Medina <rafael.diaz@konfio.mx>
2022-04-19 02:08:46 -0500

    Segunda Entrega

--- Proyecto.py
+++ Proyecto.py
@@ -1,10 +1,11 @@
 #* Rafael Díaz Medina A01024592
+
 from datetime import datetime
 
-# Opening a file
-file1 = open('portfolio.txt', 'w')
+file1 = open('portfolio.txt', 'a')
 
-actionType = input("Qué tipo de producto compraste\n 1* Forward\n 2* Call/Put\n 3* Nota Estructurada\n")
+actionType = input("Qué tipo de producto compraste\n 1* Forward\n 2* Call/Put\n 3* Nota Estructurada\n 4* Buscar Average \n")
+
 
 def forward():
     file1.writelines("Forward \n")
@@ -18,11 +19,11 @@ def forward():
     file1.writelines("Comisión: "+fee+"\n")
 
     dateIf = input("Compraste la acción hoy? ")
-    if dateIf == "y":
+    if dateIf == "y" or "Y":
         ndate = datetime.today().strftime('%Y-%m-%d')
         file1.writelines("Fecha de Compra: "+ndate+"\n")
     else:
-        date = input("Dime la fecha de cuando compraste la acción: ")
+        date = input("Escribe la fecha en que compraste la acción en el siguiente formato Y-M-D (2022-02-28): \n")
         file1.writelines("Fecha de Compra: "+date+"\n")
 
     file1.writelines("\n")
@@ -37,6 +38,11 @@ def put():
 def notaEstructurada():
     print("Nota Estructurada")
 
+def searchAveragePrice():
+    file2 = open('portfolio.txt', 'r')
+    if 'CME' in file2.read():
+        print("true")
+
 
 if actionType == "1":
     forward()
@@ -44,6 +50,8 @@ elif actionType == "2":
     call()
 elif actionType == "3":
     notaEstructurada()
+elif actionType == "4":
+    searchAveragePrice()
 else:
     print("Ese producto no existe")
 
